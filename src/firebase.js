d5676c227d4a48df82022b461d11f415fab5304a
Aditya Sharma <78463105+Aditya-Sharma-dev@users.noreply.github.com>
2021-09-30 21:57:54 +0530

    firebase initialized and deployed

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD91O19PY-3tCfcwAm0bgKkC1y-fn1bOO8",
+    authDomain: "text-player-56f39.firebaseapp.com",
+    projectId: "text-player-56f39",
+    storageBucket: "text-player-56f39.appspot.com",
+    messagingSenderId: "780716543684",
+    appId: "1:780716543684:web:bbb9c8fa146783b2adc391",
+    measurementId: "G-G9YT56LX6Z"
+};
